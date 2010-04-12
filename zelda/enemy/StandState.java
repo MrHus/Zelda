e75bc5663a9d0a53e6d31833e783bfe3e66ce56a
Vincent Klarholz <vincentklarholz@Macbook-Pro-msr911.local>
2010-04-12 13:29:12 +0200

    eye view moved to patrolbehavior

--- zelda/enemy/StandState.java
+++ zelda/enemy/StandState.java
@@ -1,11 +1,7 @@
 package zelda.enemy;
 
-import java.awt.Polygon;
-import java.awt.geom.Area;
-import zelda.engine.GObject;
 import zelda.karacter.Direction;
 import zelda.karacter.KaracterState;
-import zelda.link.Link;
 
 /**
  *
@@ -21,7 +17,6 @@ public class StandState extends KaracterState
 	private Direction oldDirection;
 	private BlueSoldier soldier;
 
-    private Polygon eyeView;
     private int oldX, oldY;
 
 	public StandState(BlueSoldier soldier)
@@ -39,30 +34,18 @@ public class StandState extends KaracterState
 		{
 			case UP:
 				soldier.setAnimation(upAnimation);
-                int[] evxposup = {karacter.getX(), karacter.getX() - 30, karacter.getX() - 20, karacter.getX() + 35, karacter.getX() + 45, karacter.getX() + 15};
-                int[] evyposup = {karacter.getY(), karacter.getY() - 40, karacter.getY() - 50, karacter.getY() - 50, karacter.getY() - 40, karacter.getY()};
-                eyeView = new Polygon(evxposup, evyposup, evxposup.length);
 				break;
 
 			case DOWN:
 				soldier.setAnimation(downAnimation);
-                int[] evxposdown = {karacter.getX(), karacter.getX() - 30, karacter.getX() - 20, karacter.getX() + 35, karacter.getX() + 45, karacter.getX() + 15};
-                int[] evyposdown = {karacter.getY() + karacter.getHeight(), karacter.getY() + karacter.getHeight() + 40, karacter.getY() + karacter.getHeight() + 50, karacter.getY() + karacter.getHeight() + 50, karacter.getY() + karacter.getHeight() + 40, karacter.getY() + karacter.getHeight()};
-                eyeView = new Polygon(evxposdown, evyposdown, evxposdown.length);
 				break;
 
 			case LEFT:
 				soldier.setAnimation(leftAnimation);
-                int[] evxposleft = {karacter.getX(), karacter.getX() - 40, karacter.getX() - 50, karacter.getX() - 50, karacter.getX() - 40, karacter.getX()};
-                int[] evyposleft = {karacter.getY() + 20 , karacter.getY() + 50, karacter.getY() + 40, karacter.getY() - 15, karacter.getY() - 25, karacter.getY() + 5};
-                eyeView = new Polygon(evxposleft, evyposleft, evxposleft.length);
 				break;
 
 			case RIGHT:
 				soldier.setAnimation(rightAnimation);
-                int[] evxposright = {karacter.getX() + karacter.getWidth(), karacter.getX() + karacter.getWidth() + 40, karacter.getX() + karacter.getWidth() + 50, karacter.getX() + karacter.getWidth() + 50, karacter.getX() + karacter.getWidth() + 40, karacter.getX() + karacter.getWidth()};
-                int[] evyposright = {karacter.getY() + 20, karacter.getY() + 50, karacter.getY() + 40, karacter.getY() - 15, karacter.getY() - 25, karacter.getY() + 5};
-                eyeView = new Polygon(evxposright, evyposright, evxposright.length);
 				break;
 		}
 	}
@@ -70,26 +53,10 @@ public class StandState extends KaracterState
 	@Override
 	public void handleInput()
 	{
-        //Add Eye View
-        soldier.getGame().getScene().addEyeView(eyeView);
-        
-        for (GObject obj : soldier.getGame().getScene().getGObjects())
-		{
-            final Area area = new Area();
-            area.add(new Area(eyeView));
-            area.intersect(new Area(obj.getRectangle()));
-
-            if((obj instanceof Link) && !area.isEmpty())
-            {
-                System.out.println("Link was seen");
-            }
-		}
-
 		//System.out.println("oldDirection " + oldDirection + " new " + karacter.getDirection());
 		if (oldDirection != karacter.getDirection())
         {
             karacter.setState(new WalkState(soldier));
-            soldier.getGame().getScene().removeEyeView(eyeView);
         }
 	}
 }
--- zelda/enemy/WalkState.java
+++ zelda/enemy/WalkState.java
@@ -25,8 +25,6 @@ public class WalkState extends KaracterState
     private int oldX, oldY;
     private long oldAnimationInterval;
 
-    private Polygon eyeView;
-
     public WalkState(BlueSoldier soldier)
 	{
 		super(soldier);
@@ -34,55 +32,29 @@ public class WalkState extends KaracterState
 
         oldX = karacter.getX();
 		oldY = karacter.getY();
-        oldAnimationInterval = karacter.getAnimationInterval();    
+        oldAnimationInterval = karacter.getAnimationInterval();
 	}
 
 	@Override
 	public void handleInput()
 	{
-        karacter.getGame().getScene().removeEyeView(eyeView);
-
-		switch (karacter.getDirection())
+  		switch (karacter.getDirection())
 		{
 			case UP:
-                int[] evxposup = {karacter.getX(), karacter.getX() - 30, karacter.getX() - 20, karacter.getX() + 35, karacter.getX() + 45, karacter.getX() + 15};
-                int[] evyposup = {karacter.getY(), karacter.getY() - 40, karacter.getY() - 50, karacter.getY() - 50, karacter.getY() - 40, karacter.getY()};
-                eyeView = new Polygon(evxposup, evyposup, evxposup.length);
                 up();
 				break;
 
 			case DOWN:
-                int[] evxposdown = {karacter.getX(), karacter.getX() - 30, karacter.getX() - 20, karacter.getX() + 35, karacter.getX() + 45, karacter.getX() + 15};
-                int[] evyposdown = {karacter.getY() + karacter.getHeight(), karacter.getY() + karacter.getHeight() + 40, karacter.getY() + karacter.getHeight() + 50, karacter.getY() + karacter.getHeight() + 50, karacter.getY() + karacter.getHeight() + 40, karacter.getY() + karacter.getHeight()};
-                eyeView = new Polygon(evxposdown, evyposdown, evxposdown.length);
                 down();
                 break;
 
 			case LEFT:
-                int[] evxposleft = {karacter.getX(), karacter.getX() - 40, karacter.getX() - 50, karacter.getX() - 50, karacter.getX() - 40, karacter.getX()};
-                int[] evyposleft = {karacter.getY() + 20 , karacter.getY() + 50, karacter.getY() + 40, karacter.getY() - 15, karacter.getY() - 25, karacter.getY() + 5};
-                eyeView = new Polygon(evxposleft, evyposleft, evxposleft.length);
 				left();
                 break;
 
 			case RIGHT:
-                int[] evxposright = {karacter.getX() + karacter.getWidth(), karacter.getX() + karacter.getWidth() + 40, karacter.getX() + karacter.getWidth() + 50, karacter.getX() + karacter.getWidth() + 50, karacter.getX() + karacter.getWidth() + 40, karacter.getX() + karacter.getWidth()};
-                int[] evyposright = {karacter.getY() + 20, karacter.getY() + 50, karacter.getY() + 40, karacter.getY() - 15, karacter.getY() - 25, karacter.getY() + 5};
-                eyeView = new Polygon(evxposright, evyposright, evxposright.length);
-				right();
+                right();
                 break;
-		}
-
-        for (GObject obj : karacter.getGame().getScene().getGObjects())
-		{
-            final Area area = new Area();
-            area.add(new Area(eyeView));
-            area.intersect(new Area(obj.getRectangle()));
-
-            if((obj instanceof Link) && !area.isEmpty())
-            {
-                System.out.println("Link was seen");
-            }
 		}
 	}
 
@@ -95,7 +67,6 @@ public class WalkState extends KaracterState
 			karacter.setDirection(Direction.LEFT);
 
 		karacter.setX(karacter.getX() - WALK_SPEED);
-        karacter.getGame().getScene().addEyeView(eyeView);
 	}
 
 	public void right()
@@ -107,7 +78,6 @@ public class WalkState extends KaracterState
 			karacter.setDirection(Direction.RIGHT);
 
 		karacter.setX(karacter.getX() + WALK_SPEED);
-        karacter.getGame().getScene().addEyeView(eyeView);
 	}
 
 	public void up()
@@ -119,7 +89,6 @@ public class WalkState extends KaracterState
 			karacter.setDirection(Direction.UP);
 
 		karacter.setY(karacter.getY() - WALK_SPEED);
-        karacter.getGame().getScene().addEyeView(eyeView);
 	}
 
 	public void down()
@@ -131,7 +100,6 @@ public class WalkState extends KaracterState
 			karacter.setDirection(Direction.DOWN);
 
 		karacter.setY(karacter.getY() + WALK_SPEED);
-        karacter.getGame().getScene().addEyeView(eyeView);
 	}
 
     @Override
