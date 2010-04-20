eb9a8a931360c02fec0df7fd2afab4ce341f22fe
Vincent Klarholz <vincentklarholz@Macbook-Pro-msr911.local>
2010-04-20 12:58:08 +0200

    merge errors

--- zelda/engine/Game.java
+++ zelda/engine/Game.java
@@ -23,7 +23,7 @@ public class Game
 {
 	private boolean running = true;
 	private boolean paused  = false;
-	private boolean debug   = true;
+	private boolean debug   = false;
 
     private int gameSpeed = 10;
     private int width = 500;
